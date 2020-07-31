d3386d117d9c39425690e0668cb57fd78506c775
anshalchauhan <59960661+anshalchauhan@users.noreply.github.com>
2020-07-31 19:51:02 +0530

    next2

---
+++ app/src/main/java/com/example/ibmhackathon2020/customer_add_details.java
@@ -0,0 +1,14 @@
+package com.example.ibmhackathon2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class customer_add_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_add_details);
+    }
+}
